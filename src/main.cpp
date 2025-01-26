7cd25c1167e53577c36f1e405a30d4b1c4c5f742
rubioanthony <rubioan@oregonstate.edu>
2025-01-26 11:43:16 -0800

    initial

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println("Hello world!");
+delay(1000);
+}
+
+// put function definitions here:
+
